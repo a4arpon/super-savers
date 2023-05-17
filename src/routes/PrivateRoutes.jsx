7249a971ff97750e14574bda6c4e654038c25d72
Mr Wayne <a4arpon@gmail.com>
2023-05-17 21:38:59 +0600

    Functionality: Private Protected Routes Added

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,19 +1,19 @@
 import { useContext } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
-import Loader from '../components/shared/Loader'
+import Loading from '../components/Loading'
 import { AuthContext } from '../context/AuthProvider'
 
 const PrivateRoutes = ({ children }) => {
   const { user, loading } = useContext(AuthContext)
   const location = useLocation()
   if (user) {
-    // when the user is available it allows user to browse users authorized pages.
+    // when user is available it allows user to browse user's authorized routes.
     return children
   } else if (loading) {
-    // While application is checking users state it shows a loader.
-    return <Loader />
+    // While firebase is checking users state it shows a spinner.
+    return <Loading />
   } else {
-    // If the user is not available private route will be redirect to login page
+    // If the user is not available private route will redirect user to login page
     return <Navigate to={'/login'} replace state={{ from: location }} />
   }
 }
