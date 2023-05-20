2b54fa3394113e8e12a6666aae2acf797a2e2eb8
Mr Wayne <a4arpon@gmail.com>
2023-05-21 04:08:24 +0600

    Feature: Toy Can Be Added to DB From Now

--- src/pages/addToy/AddToy.jsx
+++ src/pages/addToy/AddToy.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable camelcase */
+import { useContext } from 'react'
+import { toast } from 'react-toastify'
+import { AuthContext } from '../../contexts/AuthProvider'
 import useTitle from '../../hooks/useTitle'
 
 const AddToy = () => {
   useTitle('Add New Toy | SuperSaver')
-  const handleToySubmit = (e) => {
+  const { user } = useContext(AuthContext)
+  const handleToySubmit = async (e) => {
     e.preventDefault()
     const form = e.target
     const title = form.title.value
@@ -12,16 +17,33 @@ const AddToy = () => {
     const category = form.category.value
     const details = form.details.value
     const rating = form.rating.value
+    const seller = form.seller.value
+    const seller_email = form.seller_email.value
     const toyData = {
       title,
       img,
-      price,
+      price: parseFloat(price).toFixed(2),
       quantity,
       category,
       details,
-      rating
+      rating: parseFloat(rating).toFixed(1),
+      seller,
+      seller_email
     }
-    console.log(toyData)
+    const response = await fetch(
+      'https://b7a11-toy-marketplace-server-side-a4arpon.vercel.app/toy/add',
+      {
+        method: 'POST',
+        body: JSON.stringify(toyData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    if (response?.status === 200) {
+      toast.success('Toy Added Successfully')
+      form.reset()
+    }
   }
   return (
     <div className="my-20 container mx-auto p-2">
@@ -34,6 +56,7 @@ const AddToy = () => {
             <label>Enter Toy Name</label>
             <input
               type="text"
+              required
               className="input input-bordered w-full mt-2"
               name="title"
             />
@@ -42,6 +65,7 @@ const AddToy = () => {
             <label>Enter Toy Price</label>
             <input
               type="text"
+              required
               className="input input-bordered w-full mt-2"
               name="price"
             />
@@ -50,6 +74,7 @@ const AddToy = () => {
             <label>Enter Toy Ratings</label>
             <input
               type="text"
+              required
               defaultValue="3.5"
               className="input input-bordered w-full mt-2"
               name="rating"
@@ -59,6 +84,7 @@ const AddToy = () => {
             <label>Enter Toy Img Link</label>
             <input
               type="text"
+              required
               className="input input-bordered w-full mt-2"
               name="img"
             />
@@ -69,15 +95,18 @@ const AddToy = () => {
               className="select select-bordered w-full mt-2"
               name="category"
             >
-              <option disabled>Select A Toy Category</option>
-              <option value="20">Avengers Toy</option>
-              <option value="15">Titans Toy</option>
-              <option value="10">Justice League Toy</option>
+              <option disabled selected>
+                Select A Toy Category
+              </option>
+              <option value="avengers">Avengers Toy</option>
+              <option value="Titans">Titans Toy</option>
+              <option value="Justice League">Justice League Toy</option>
             </select>
             <div className="mb-2 mt-3">
               <label>Enter Toy Quantity</label>
               <input
                 type="text"
+                required
                 className="input input-bordered w-full mt-2"
                 name="quantity"
               />
@@ -87,11 +116,33 @@ const AddToy = () => {
             <label>Enter Toy Details</label>
             <textarea
               name="details"
+              required
               className="textarea textarea-bordered w-full mt-2"
               rows="5"
             ></textarea>
           </div>
+          <div className="mb-2">
+            <label>Enter Your Name(Seller Name)</label>
+            <input
+              type="text"
+              required
+              className="input input-bordered w-full mt-2"
+              name="seller"
+              defaultValue={user?.displayName || ''}
+            />
+          </div>
+          <div className="mb-2">
+            <label>Enter Your Email(Seller Email)</label>
+            <input
+              type="text"
+              required
+              className="input input-bordered w-full mt-2"
+              name="seller_email"
+              defaultValue={user?.email || ''}
+            />
+          </div>
         </div>
+
         <div className="w-full text-center mt-4">
           <button
             className="btn btn-primary btn-outline border-2 w-full lg:w-1/2 font-semibold"
