bdc8b093bd1bfd73364384d2519673f2a8cb7fac
Mr Wayne <a4arpon@gmail.com>
2023-05-21 23:12:28 +0600

    Fix: Loader Added

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,10 @@
+import imgLoad from '../assets/Loader.svg'
 const Loading = () => {
-  return <div>Loading</div>
+  return (
+    <div className="h-[calc(100vh-30vh)] container mx-auto flex justify-center items-center">
+      <img src={imgLoad} alt="" />
+    </div>
+  )
 }
 
 export default Loading
